9c2b6c89e7b36abc6c5d153d1407b231e316fc1c
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-18 12:33:32 +0545

    Add and configure reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksSlice from './books/booksSlice';
-import categoriesSlice from './categories/categoriesSlice';
+import categoriesSlice from './Categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
