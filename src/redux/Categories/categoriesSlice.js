9df2e7fdd6866f41b76c4e1cf148b9f8350ad2d3
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-18 09:35:48 +0545

    Define slice of state for books and categories

---
+++ src/redux/Categories/categoriesSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState: {
+        categories: [],
+        status: "Under Construction",
+    },
+    reducers: {
+        categoryStatus: (state) => state.status,
+        },
+});
+
+export const { addCategory } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
