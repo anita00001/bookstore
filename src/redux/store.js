0e3fae897e4aed418a3570308a50fd78a68ba02b
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-18 10:00:31 +0545

    Fix linters missing file extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import booksReducer from './books/booksSlice';
-import categoriesReducer from './categories/categoriesSlice';
+import booksReducer from './books/booksSlice.js';
+import categoriesReducer from './categories/categoriesSlice.js';
+
+// import booksReducer from './books/booksSlice';
+// import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
