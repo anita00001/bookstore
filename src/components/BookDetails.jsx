7bd7217cc8d41e67cf324d1a9c2b1f51da44582f
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-18 15:52:16 +0545

    Display books receive from slice, in react component

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -5,66 +5,72 @@
 // |   |-- ProgressDetails
 // |-- InputBook
 
-const bookList = [
-  {
-    id: 1,
-    category: 'Action',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: 2,
-    category: 'Science Fiction',
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: 3,
-    category: 'Economy',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-  },
-];
+import { useSelector } from 'react-redux';
 
-const BookDetails = () => (
-  <div>
-    {bookList.map((book) => (
-      <ul
-        key={book.id}
+// const bookList = [
+//   // {
+//   //   id: 1,
+//   //   category: 'Action',
+//   //   title: 'The Hunger Games',
+//   //   author: 'Suzanne Collins',
+//   // },
+//   // {
+//   //   id: 2,
+//   //   category: 'Science Fiction',
+//   //   title: 'Dune',
+//   //   author: 'Frank Herbert',
+//   // },
+//   // {
+//   //   id: 3,
+//   //   category: 'Economy',
+//   //   title: 'Capital in the Twenty-First Century',
+//   //   author: 'Suzanne Collins',
+//   // },
+// ];
+
+const BookDetails = () => {
+  const bookList = useSelector((state) => state.books);
+
+  return (
+    <div>
+      {bookList.map((book) => (
+        <ul
+          key={book.item_id}
+        >
+          <li>
+            Category:
+            {'  '}
+            {book.category}
+          </li>
+          <li>
+            Book Title:
+            {'  '}
+            {book.title}
+          </li>
+          <li>
+            Book Author:
+            {'  '}
+            {book.author}
+          </li>
+        </ul>
+      ))}
+      <button
+        type="submit"
       >
-        <li>
-          Category:
-          {'  '}
-          {book.category}
-        </li>
-        <li>
-          Book Title:
-          {'  '}
-          {book.title}
-        </li>
-        <li>
-          Book Author:
-          {'  '}
-          {book.author}
-        </li>
-      </ul>
-    ))}
-    <button
-      type="submit"
-    >
-      Comments
-    </button>
-    <button
-      type="submit"
-    >
-      Remove
-    </button>
-    <button
-      type="submit"
-    >
-      Edit
-    </button>
-  </div>
-);
+        Comments
+      </button>
+      <button
+        type="submit"
+      >
+        Remove
+      </button>
+      <button
+        type="submit"
+      >
+        Edit
+      </button>
+    </div>
+  );
+};
 
 export default BookDetails;
